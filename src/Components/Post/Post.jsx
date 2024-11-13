9adff0585ee7f8fe6c6d7aeeb0b98321d4e95ab4
fardinariful <fardinewucse2020@gmail.com>
2024-11-13 17:19:00 +0600

    navigation added

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Post.css'
 const Post = ({post}) => {
     const {id,title,body}=post;
+        const navigate=useNavigate();
+    const handleclick1=()=>{
+        navigate(`/post/${id}`);
+    }
     return (
         <div className="poststyle">
             <h3>Userid:{id}</h3>
             <p>{title}</p>
             <p>{body}</p>
              <Link to={`/post/${id}`}>Show Details</Link>
+             <br />
+             <button onClick={handleclick1}>Click me for details</button>
         </div>
     );
 };
--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,13 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
     const PostDetail=useLoaderData();
+    const navigate=useNavigate();
     const {id,title,body}=PostDetail;
+    const handleclick2=()=>{
+      navigate(-1);
+    }
     return (
         <div>
             <h3>ID:{id}</h3>
             <p>{title}</p>
             <p>{body}</p>
+            <button onClick={handleclick2}>GO Back</button>
         </div>
     );
 };
